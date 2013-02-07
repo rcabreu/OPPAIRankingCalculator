5ec9af7fcd5800b364d02a4a647a5fb4b0399972
Roberto Abreu <rcabreu@gmail.com>
2013-02-06 22:10:57 -0400

    updating

--- Program.java
+++ Program.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Program {
 	public static void main(String ... args) throws Exception {
-		Contest contest = XMLContestReader.parseContestFile("C:\\RankingCalculator\\oppai4.xml");
+		Contest contest = XMLContestReader.parseContestFile(args[0]);
 		contest.updateRanking(new PositionalEloCalculator());
 		ArrayList<Contestant> list = contest.getContestants();
 		for (Contestant c : list) {
