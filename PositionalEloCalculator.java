3d110c7cf46640ac46526533cbdcccf4b5306f56
Roberto Abreu <rcabreu@gmail.com>
2013-10-21 19:32:35 -0400

    Fixed bug

--- PositionalEloCalculator.java
+++ PositionalEloCalculator.java
@@ -1,12 +1,12 @@
 import java.util.*;
 public class PositionalEloCalculator implements RankingCalculationStrategy {
 	
-	public static final double K = 20.0;
+	public static final double K = 20;
 	public static final double INITIAL = 1200;
 	
 	public void calculate(Contest contest) {
 		ArrayList<Contestant> contestants = contest.getContestants();
-		double firstSeed = contestants.size() /2 + 1;
+		double firstSeed = contestants.size() / 2.0;
 	
 		for (int i = 0; i < contestants.size(); ++i) {
 			Contestant a = contestants.get(i);
@@ -16,10 +16,12 @@ public class PositionalEloCalculator implements RankingCalculationStrategy {
 	
 			for (int j = 0; j < contestants.size(); ++j) {
 				if (i == j) continue;
-				Contestant b = contestants.get(i);
+				Contestant b = contestants.get(j);
 			    double rB = b.getPreviousRanking();
 			    double pB = b.getCurrentPosition();
-			    double eA = 1.0 / ( 1 + Math.pow(10.0, (rB - rA) / 400));
+                            double eA = 0.0D;
+                            eA = Math.pow(10.0D, (rA - rB) / 400.0D);
+                            eA = 1.0D / (1.0D + eA);
 			    score += eA;
 			}
 			double seed = rA == INITIAL ? firstSeed : score;
